f8431dac933aef296ee51e5f8c00300749ed8fd7
白ちぇす <mons1.zgmf@gmail.com>
2026-06-07 12:19:28 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,6 @@
 // config.js
-const APP_VERSION = "1.1.0";
+const APP_VERSION = "2.0.0";
 
 const config = {
-  name: "博衣こより",
-  color1: "#fe68ad",
-  color2: "#f09cc0",
-  color3: "#ffacd3",
-  api: "https://script.google.com/macros/s/AKfycby35nCN7uhqkTvMhTmb52O33xHxrpHEYkjXl0h8sCv34gxUenTfXbrAwzCs86kb05AR0w/exec"
+  api: "https://script.google.com/macros/s/AKfycbyG46p8S6poahfl2Q7BbuTVb2VW-gSIEsJspMEekQQJnDPG7tqLsQsBMjboUsxy7-xK/exec"
 };
